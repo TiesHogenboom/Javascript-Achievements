3e8a98c074d32e9515733f2c7f22ecdc7cf57501
TiesHogenboom <70697022+TiesHogenboom@users.noreply.github.com>
2021-01-20 12:55:13 +0100

    quiz opdracht gemaakt

---
+++ F1M2BO-Quiz-Javascript-main/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI":" https://www.lamlisse.nl/wp-content/uploads/2018/06/tilleman_20180609-0018.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Is het musuem toegankelijk voor rolstoel- en rollatorgebruikers?",
+				"answers": [{
+						"answer": "Ja, je kan overal komen omdat er een lift is",
+						"feedback": true
+					},
+					{
+						"answer": "Alleen de eerste verdieping omdat er geen lift is",
+						"feedback": false
+					},
+					{
+						"answer": "Nee, het museum is te klein",
+						"feedback": false
+					},
+					{
+						"answer": "Geen idee",
+						"feedback": false
+					},
+					{
+						"answer": "Ja, maar het omliggende landgoed is wat moeilijker te beruiken voor hun",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Heeft de collectie een thema?",
+				"answers": [{
+						"answer": "Street art",
+						"feedback": false
+					},
+					{
+						"answer": "Food art",
+						"feedback": true
+					},
+					{
+						"answer": "Colorfull art",
+						"feedback": false
+					},
+					{
+						"answer": "Kitsch art",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is het kijktelefoon?",
+				"answers": [{
+						"answer": "Elke woensdag kan je met het LAM een gesprek doen over kunst",
+						"feedback": true
+					},
+					{
+						"answer": "Een kunstwerk in het LAM",
+						"feedback": false
+					},
+					{
+						"answer": "Een audio fragment dat door het museum afspeelt",
+						"feedback": false
+					},
+					{
+						"answer": "Een online-simulatie",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
